c83017c2f76b7452f9ef642d6e9e674ebda90260
Nicole <silvanicol830@gmail.com>
2021-09-01 15:40:21 -0300

    Resuelto el ejercicio 1

--- main.js
+++ main.js
@@ -35,8 +35,9 @@ function draw() {
   if (hizoClick == true)
   {
     pintarCasillero(columnaPresionada, filaPresionada, COLOR_CASILLERO_SIN_MINA); //pinta el casillero clickeado. Modificar/completar
-
-
+      if(tieneMinaCasillero(columnaPresionada, filaPresionada)){
+        perder
+      }
     
     hizoClick = false;  //Indico que ya "procesé" el click del usuario. NO modificar
   }
